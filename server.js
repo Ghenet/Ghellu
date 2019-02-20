50af0dde3c4c4d564d93c4560f91d4ee22ebd039
Abimzy <eabiemo@gmail.com>
2019-02-20 14:11:32 -0800

    Updated server.js with dummy data

--- server.js
+++ server.js
@@ -22,42 +22,138 @@ app.get('/', (req, res) => {
 
 //Data
 
-var question = [{
-        title: "Who",
+let question = [{
         _id: 1,
+        description: "What is html"
     },
     {
-        title: "What",
         _id: 2,
+        description: "What is css"
     },
     {
-        title: "when",
         _id: 3,
-    },
-];
-
-var answer = [{
-        title: "",
-        _id: 1,
-        value: "This is the answer",
-        img: ""
+        description: "Why use cors"
     },
     {
-        title: "",
-        _id: 2,
-        value: "This is the answer",
-        img: ""
+        _id: 4,
+        description: "What is mongoose"
     },
     {
-        title: "",
-        _id: 3,
-        value: "This is the answer",
-        img: ""
+        _id: 5,
+        description: "Why use package.json"
+    },
+    {
+        _id: 6,
+        description: "What is css"
+    },
+    {
+        _id: 7,
+        description: "What is css"
+    },
+    {
+        _id: 8,
+        description: "What is css"
+    },
+    {
+        _id: 9,
+        description: "What is css"
+    },
+    {
+        _id: 10,
+        description: "What is css"
+    },
+    {
+        _id: 11,
+        description: "What is css"
+    },
+    {
+        _id: 12,
+        description: "What is css"
+    },
+    {
+        _id: 13,
+        description: "What is css"
+    },
+    {
+        _id: 14,
+        description: "What is css"
+    },
+    {
+        _id: 15,
+        description: "What is css"
     }
 ];
+
+let answer = [{
+        _id: 1,
+        description: "response 1"
+    },
+    {
+        _id: 2,
+        description: "text 2"
+    },
+    {
+        _id: 3, 
+        description: "text4"
+    },
+    {
+        _id: 4,
+        description: "What is mongoose"
+    },
+    {
+        _id: 5,
+        description: "Why use package.json"
+    },
+    {
+        _id: 6,
+        description: "What is css"
+    },
+    {
+        _id: 7,
+        description: "What is css"
+    },
+    {
+        _id: 8,
+        description: "What is css"
+    },
+    {
+        _id: 9,
+        description: "What is css"
+    },
+    {
+        _id: 10,
+        description: "What is css"
+    },
+    {
+        _id: 11,
+        description: "What is css"
+    },
+    {
+        _id: 12,
+        description: "What is css"
+    },
+    {
+        _id: 13,
+        description: "What is css"
+    },
+    {
+        _id: 14,
+        description: "What is css"
+    },
+    {
+        _id: 15,
+        description: "What is css"
+    }
+];
+
+//Create CRUD endpoints
+
+//Create reviews (post method)
+//Update reviews(ge)
+
+
 
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Ghellu game listening at http://localhost:3000/`)
 });
-
