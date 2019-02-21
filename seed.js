09e4f571dbec03bd92cb08c7a6e960d81f696c32
Abimzy <eabiemo@gmail.com>
2019-02-21 11:45:39 -0800

    Created dummy data to seed database

--- seed.js
+++ seed.js
@@ -1,5 +1,62 @@
+//Import all models here
 const db = require('./models');
 
-let $questionList = [
 
-];
+let questionList = [{
+        description: "question 1"
+    },
+    {
+        description: "question 2"
+    },
+    {
+        description: "question 3"
+    },
+    {
+        description: "question 4"
+    },
+    {
+        description: "question 5"
+    },
+    {
+        description: "question 6"
+    },
+    {
+        description: "question 7"
+    },
+    {
+        description: "question 8"
+    },
+    {
+        description: "question 9"
+    },
+    {
+        description: "question 10"
+    },
+    {
+        description: "question 11"
+    },
+    {
+        description: "question 12"
+    },
+    {
+        description: "question 13"
+    },
+    {
+        description: "question 14"
+    },
+    {
+        description: "question 15"
+    }
+]
+
+//Making sure first dummy data works before creating additional ones
+// let answerList = [{
+
+// },
+// ]
+
+// let reviewsList = [{
+
+// },
+// ]
+
