97b5d26fc9d4d2601555f81ae5c98fd6dd2e74b3
Abimzy <eabiemo@gmail.com>
2019-02-23 15:34:21 -0800

    Created app.js to store event listeners

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    //GAME FUNCTIONALITY
+    //Pseudo code
+    //When user clicks the answer button and the answer id matches the question id, on click of check button, increment score by 1
+    //If the answer button clicked is the wrong one, the check button turns red, and button value changes to 'try again'
+    //Event listeners
+    //Use Math.random();
+});
